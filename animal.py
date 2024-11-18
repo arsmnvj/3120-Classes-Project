5e1d944ee78bab52c37c062c6fd2c408bcde7b2f
Arsim Novaj <ArsimNovaj@Arsims-MacBook-Air-2.local>
2024-11-17 22:55:04 -0500

    initial commit

---
+++ animal.py
@@ -0,0 +1,14 @@
+class Animal:
+	def __init__(self, species, legs, fur):
+		self.species = species
+		self.legs = legs
+		self.fur = fur
+
+	def walk(self):
+		pass
+
+	def eat(self):
+		pass
+
+	def sleep(self):
+		pass 
